80d427800d4d88d5c968420b2c35e34a1263ee19
rohitwithcode <radharani7253@gmail.com>
2023-10-23 17:07:51 +0530

    Hello World Program

--- src/Chapter_1_Datatypes_and_Variables/Main.java
+++ src/Chapter_1_Datatypes_and_Variables/Main.java
@@ -2,7 +2,6 @@ package Chapter_1_Datatypes_and_Variables;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to CodeWithRohit Education PlatForm");
         System.out.println("Hello world!");
     }
 }
