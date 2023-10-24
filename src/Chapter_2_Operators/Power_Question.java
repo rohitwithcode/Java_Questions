5ba22dcc965ba8b257e2fee4ae764ca64dfaad7f
rohitwithcode <radharani7253@gmail.com>
2023-10-24 07:18:51 +0530

    Math.Power Question Program

---
+++ src/Chapter_2_Operators/Power_Question.java
@@ -0,0 +1,11 @@
+package Chapter_2_Operators;
+import  java.lang.Math.*;
+public class Power_Question {
+    public static void main(String[] args) {
+
+        int number =7;
+        int power=5;
+
+        System.out.println("The "+number+" after power of "+power+" is : "+Math.pow(number,power));
+    }
+}
