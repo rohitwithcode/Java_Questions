2c8720ac213fa740d182f7c31207d83ffb07e7ff
rohitwithcode <radharani7253@gmail.com>
2023-10-24 07:09:09 +0530

    Double Equal Operator Program

---
+++ src/Chapter_2_Operators/Equality_Check_Operator.java
@@ -0,0 +1,15 @@
+package Chapter_2_Operators;
+
+/*
+        Write a program to check if the two numbers 23 and 45 are equal.
+ */
+public class Equality_Check_Operator {
+    public static void main(String[] args) {
+
+        int firstNumber=23;
+        int secondNumber=45;
+
+        System.out.println("Are "+firstNumber+" and "+secondNumber+" Equal : "+(firstNumber==secondNumber));
+
+    }
+}
