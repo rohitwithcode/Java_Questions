7a05dcacff9110207c499beaa5731d7a0fc59c91
rohitwithcode <radharani7253@gmail.com>
2023-10-25 07:49:49 +0530

    Combine two String without concatenate method Program

---
+++ src/Chapter_3_Strings/Combine_two_string_without_concatenate_method.java
@@ -0,0 +1,21 @@
+package Chapter_3_Strings;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Combine_two_string_without_concatenate_method {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter your First string : ");
+        String first=scanner.nextLine();
+
+        System.out.println("Enter your Second string : ");
+        String second=scanner.nextLine();
+
+        String combine=first+second;
+
+        System.out.println("Your combine string : "+combine);
+    }
+}
